513f4f96704b1239ec76e40e5b90fff0a07aec26
augustanshu <2430972123@qq.com>
2015-12-23 16:53:55 +0800

    Signed-off-by: augustanshu <2430972123@qq.com> database/seeds/

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class AdminTableSeeder extends Seeder
 {
@@ -11,6 +12,10 @@ class AdminTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('admins')->insert([
+		'name'=>'admin',
+		'password'=>bcrypt('123'),
+		'role'=>'leader',
+		]);
     }
-}
+} 
