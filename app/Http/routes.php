7aca8189189185ba3a8ea8c66215d6c2854931ba
augustanshu <2430972123@qq.com>
2015-12-23 16:53:15 +0800

    Signed-off-by: augustanshu <2430972123@qq.com> 12.23

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,61 +10,25 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/1','Test1Controller@index');
 Route::get('/','HomeController@index');
 Route::get('home','HomeController@index');
-Route::group(['prefix'=>'test','middleware'=>'auth','namespace'=>'Test'],function(){
-	Route::get('/','TestController@index');
-	Route::post('/p',function(){ return "hello world";});
-});
-/*
-Route::get('test',[
- 'middleware'=>'auth',
- 'uses'=>'TestController@index'
-]);
-*/
 
 Route::get('auth/login','Auth\AuthController@getLogin');
 Route::post('auth/login','Auth\AuthController@postLogin');
 Route::get('auth/logout','Auth\AuthController@getLogout');
-
 Route::get('auth/register','Auth\AuthController@getRegister');
 Route::post('auth/register','Auth\AuthController@postRegister');
-/*
-Route::controllers([
-'auth'=>'Auth\AuthController',
-]);
-*/
-Route::get('/test/1',['as'=>'academy',function(){
-	return "1231";
-}]);
-Route::get('/test',function(){
-	return redirect()->route('academy');
+
+Route::group(['prefix'=>'test','namespace'=>'Test'],function(){
+	Route::get('2','TestController@index');
+	Route::get('1',function(){return '123';});
 });
 
-Route::get('/hello/laravelacademy/{id}',['as'=>'academy',function($id){
-	return 'hello'.$id.'!';
-}]);
-Route::get('/testnamelaravel',function(){
-	return redirect()->route('academy',['id'=>4]);
+Route::group(['prefix'=>'admin','namespace'=>'Admin'],function(){
+	resource('/','PostController');
+	resource('post','PostController');
+	resource('set','SetController');
+	resource('products','ProductController');
+	resource('users','UsersController');
+	resource('business','BusinessController');
 });
-Route::group(['as'=>'admin'],function(){
-	Route::get('dadasda',['as'=>'dadasda',function(){
-		return '123131';
-	}]);
-});
-Route::get('/testnameroute',function(){
-	return route('admin::dadasda');
-});
-Route::group(['middleware'=>'test:male'],function(){
-	Route::get('/write/person1',function(){
-		
-	});
-	Route::get('/write/person2',function(){
-		
-	});
-});
-Route::get('/age/refuse',['as'=>'refuse',function(){
-	return "未成年人禁止入内";
-}]);
-
